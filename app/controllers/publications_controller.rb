e0d04505ec2c0031c17f94278e7bd7bc817cf2d7
Oleg Vodopyan <oleg.vodopyan@gmail.com>
2012-06-10 17:46:19 +0300

    ed

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -76,7 +76,7 @@ class PublicationsController < ApplicationController
     @publication.destroy
 
     respond_to do |format|
-      format.html { redirect_to Publications_url }
+      format.html { redirect_to publications_url }
       format.json { head :no_content }
     end
   end
