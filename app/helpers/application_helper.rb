5ade8fa0a0ecf82dfb0d5dbd40b01342be5e09d6
Oleg Vodopyan <oleg.vodopyan@gmail.com>
2012-06-13 03:13:25 +0300

    another fix part

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
 		true			
 	end
 
+	def domain
+		"http://qqpokerface.com"
+	end
+
+	def social_buttons url
+		@url = url
+		render :template => "_partial/social_buttons"
+	end
 end
